c9732e2cd39488e84e9f537e9a6ae1c1f5218b10
Edgar Tinajero <cetinajero@gmail.com>
2018-05-12 08:39:51 -0600

    Delete unneded spaces

--- Tutorial_ESP8266_Arduino_1.ino
+++ Tutorial_ESP8266_Arduino_1.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 
- #define LED 2
+#define LED 2
 
 const char* ssid     = "Sala";
 const char* password = "Pitufo.loco";
@@ -9,21 +9,18 @@ const char* password = "Pitufo.loco";
 WiFiServer servidorTCP(8266);
 WiFiClient clienteTCP;
 
-
-
 void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
-  
+
   Serial.begin(115200);
   delay(100);
-  
+
   Serial.print("Conectandose a: ");
   Serial.println(ssid);
 
-
   WiFi.begin(ssid, password);  //Intentamos conectarnos a la red Wifi
-  
+
   while (WiFi.status() != WL_CONNECTED) {  //Esperamos hasta que se conecte.
     delay(200);
   }
@@ -44,27 +41,16 @@ void loop() {
         // read data from the connected client
         if (clienteTCP.available() > 0) {
             char dato = clienteTCP.read();
-            
 
             if (dato == '1'){
               digitalWrite(LED_BUILTIN, HIGH);
               Serial.write(dato);
             }
             else if (dato == '0'){
-
               digitalWrite(LED_BUILTIN, LOW);
               Serial.write(dato);
-              
             }
-              
 
-           
-              
         }
     }
-     
-     
-     
-      
-
 }
